af87abd27177ad4079efe46a1fae4bd44c8b4d36
NodjaLima <nodjamplima@gmail.com>
2022-09-27 09:45:10 -0300

    inserida depencia dotenv e feita a config necessária

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import StudentsController from './src/controller/studentsController.js'
+import dotenv from "dotenv"
 
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
+dotenv.config();
 
 app.listen(port, ()=>{console.log(`Servidor online no endereço http://localhost:${port}`)});
 
