9055340f531dff7540a4f3cecc42d351b6ddd412
NodjaLima <nodjamplima@gmail.com>
2022-09-27 16:25:09 -0300

    insere dependecia de cors e config no app,js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
-import StudentsController from './src/controller/studentsController.js'
-import dotenv from "dotenv"
+import StudentsController from './src/controller/studentsController.js';
+import dotenv from "dotenv";
+import cors from 'cors';
 
 const app = express();
 const port =  process.env.PORT || 3000;
 dotenv.config();
+app.use(cors())
 
 app.listen(port, ()=>{console.log(`Servidor online no endereço http://localhost:${port}`)});
 
