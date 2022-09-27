aec09a89c1fdccf76ccc2123427f2ab9513d75e8
NodjaLima <nodjamplima@gmail.com>
2022-09-27 16:35:13 -0300

    corrige rotas e exclui arquivo .db

--- src/controller/studentsController.js
+++ src/controller/studentsController.js
@@ -6,7 +6,7 @@ DbMethods.createTable()
 
 class StudentsController {
   static rotas(app){
-    app.get("/studants", async (req, res) => {
+    app.get("/", async (req, res) => {
       try {
         const response = await DbMethods.getStudents()
         res.status(200).json(response)
@@ -15,7 +15,7 @@ class StudentsController {
       }  
     });
 
-    app.post("/studants", async (req, res) => {
+    app.post("/", async (req, res) => {
       try {
         const body = req.body
         const itemValid = ValidationService.validNewStudent(body.name_student, body.class_student, body.age_student)
@@ -31,7 +31,7 @@ class StudentsController {
       }      
     })
 
-    app.delete("/studants/:id", async (req, res) => {
+    app.delete("/:id", async (req, res) => {
       try {                
         const item = await DbMethods.deletStudent(req.params.id)
         res.status(200).json(item)
